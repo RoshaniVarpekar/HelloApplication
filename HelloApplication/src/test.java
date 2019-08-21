041726b582182d28eaa2291e82188496872a9e07
Roshani-Varpekar <roshani_varpekar@persistent.com>
2019-08-21 16:06:29 +0530

    Stashing

--- HelloApplication/src/test.java
+++ HelloApplication/src/test.java
@@ -2,8 +2,5 @@ public class Test{
     public static void main(String[] args) {
         System.out.println("Test class created. Hello!!!");
 
-        System.out.println("Test class created");
-
-
     }
 }
