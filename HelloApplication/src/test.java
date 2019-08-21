9782d48b8b17f194741511ee96ab5a8a6dbfeee0
Roshani-Varpekar <roshani_varpekar@persistent.com>
2019-08-21 16:04:43 +0530

    Commiting after stash

---
+++ HelloApplication/src/test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args) {
+<<<<<<< Updated upstream
+        System.out.println("Test class created. Hello!!!");
+=======
+        System.out.println("Test class created");
+>>>>>>> Stashed changes
+    }
+}
